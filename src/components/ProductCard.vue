ba2ba90111fa317d98d72c59caacb9c3fda6168f
Antonio <antonioboksic1603@gmail.com>
2023-06-11 19:06:04 +0200

    completato seconda parte milestone 3, diviso voto per 2, arrotondato per eccesso, trasformato in stelline

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -14,6 +14,12 @@ export default {
             
      }
   },
+//   methods: {
+//     transformVotesInStars(numero) {
+//         "<i class='fa-solid fa-star'></i>" * numero
+//     }
+//   }
+
 }
 
 </script>
@@ -23,26 +29,29 @@ export default {
     
     <!-- blocco prodotto -->
     <div class="prod-card">
+
         <div class="img-card">
             <img :src="'https://image.tmdb.org/t/p/w342' + details.backdrop_path" alt="immagine film">
         </div>
 
         
-            <div class="titolo-card">{{ details.title ? details.title : details.name }}</div>  
-            <!-- .name per SERIE  -->
+        <div class="titolo-card">{{ details.title ? details.title : details.name }}</div>  
+        <!-- .name per SERIE  -->
 
-            <div class="titoloOriginale-card">Titolo originale: {{ details.original_title ? details.original_title : details.original_name }}</div>
-             <!-- .original_name per SERIE  -->
+        <div class="titoloOriginale-card">Titolo originale: {{ details.original_title ? details.original_title : details.original_name }}</div>
+        <!-- .original_name per SERIE  -->
 
-            <div class="voto-card">Voto: {{ details.vote_average }}</div>
-            <!-- .vote_average per SERIE  -->
+        <div class="voto-card">Voto:
+            <i v-for="star in Math.round(details.vote_average / 2)" class='fa-solid fa-star'></i>
+        </div>
+        <!-- .vote_average per SERIE  -->
 
-            <!-- <div class="lingua-card">Lingua originale: {{ store.lingueDiCuiHoBandiere.includes(details.original_language) ? '../../public/' + details.original_language + '.png' : details.original_language }}</div> -->
-            <!-- .original_language per SERIE  -->
+        <!-- <div class="lingua-card">Lingua originale: {{ store.lingueDiCuiHoBandiere.includes(details.original_language) ? '../../public/' + details.original_language + '.png' : details.original_language }}</div> -->
+        <!-- .original_language per SERIE  -->
 
-            <span>Lingua originale:</span>
-            <img class="bandiera"
-            :src="store.lingueDiCuiHoBandiere.includes(details.original_language) ? '/' + details.original_language + '.png' : details.original_language" :alt="store.lingueDiCuiHoBandiere.includes(details.original_language) ? '/' + details.original_language + '.png' : details.original_language">
+        <span>Lingua originale:</span>
+        <img class="bandiera"
+        :src="store.lingueDiCuiHoBandiere.includes(details.original_language) ? '/' + details.original_language + '.png' : details.original_language" :alt="store.lingueDiCuiHoBandiere.includes(details.original_language) ? '/' + details.original_language + '.png' : details.original_language">
 
     </div>
 
@@ -82,6 +91,10 @@ export default {
     .bandiera {
         width: 30px;
         height: 20px;
+    }
+
+    i {
+        color: yellow
     }
 
 
