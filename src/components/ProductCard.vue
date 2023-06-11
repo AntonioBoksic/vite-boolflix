35627a914f534597ce7f5574a611d540d671ad7c
Antonio <antonioboksic1603@gmail.com>
2023-06-11 18:28:02 +0200

    prima parte di milestone 3 completata, immagini inserite correttamente

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -23,12 +23,15 @@ export default {
     
     <!-- blocco prodotto -->
     <div class="prod-card">
+        <div class="img-card">
+            <img :src="'https://image.tmdb.org/t/p/w342' + details.backdrop_path" alt="immagine film">
+        </div>
 
         
             <div class="titolo-card">{{ details.title ? details.title : details.name }}</div>  
             <!-- .name per SERIE  -->
 
-            <div class="titoloOriginale-card">{{ details.original_title ? details.original_title : details.original_name }}</div>
+            <div class="titoloOriginale-card">Titolo originale: {{ details.original_title ? details.original_title : details.original_name }}</div>
              <!-- .original_name per SERIE  -->
 
             <div class="voto-card">Voto: {{ details.vote_average }}</div>
@@ -37,6 +40,7 @@ export default {
             <!-- <div class="lingua-card">Lingua originale: {{ store.lingueDiCuiHoBandiere.includes(details.original_language) ? '../../public/' + details.original_language + '.png' : details.original_language }}</div> -->
             <!-- .original_language per SERIE  -->
 
+            <span>Lingua originale:</span>
             <img class="bandiera"
             :src="store.lingueDiCuiHoBandiere.includes(details.original_language) ? '/' + details.original_language + '.png' : details.original_language" :alt="store.lingueDiCuiHoBandiere.includes(details.original_language) ? '/' + details.original_language + '.png' : details.original_language">
 
@@ -58,6 +62,14 @@ export default {
     margin-bottom: 30px;
     height: 350px;
 
+    .img-card {
+        width: 100%;
+
+        img {
+            width: 100%;
+            height: 100%;
+        }
+    }
     
 
     .titolo-card {
